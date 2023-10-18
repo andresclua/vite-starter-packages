5052f880f9ae5804e0fb4da61eb8f42bd4f0190b
Andrés Clúa <andresclua@gmail.com>
2023-10-18 15:30:51 +0200

    update

--- decs.d.ts
+++ decs.d.ts
@@ -1,2 +1,8 @@
 declare module "@teamthunderfoot/breakpoints"
-declare module '@andresclua/jsutil'
+declare module '@andresclua/jsutil' {
+    export default class JsUtil {
+      constructor();
+      console.log('test')
+      // Other methods or properties
+    }
+  }
